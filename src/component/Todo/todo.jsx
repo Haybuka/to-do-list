1e643c14e5b8d535eeded1cebec284e3ec77c79f
haybuka <paschal.nduleo@gmail.com>
2023-06-05 17:35:41 +0100

    SETTING UP EDITING

--- src/component/Todo/todo.jsx
+++ src/component/Todo/todo.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const Todo = () => {
   const [task, setTask] = useState("");
   const [placeholder, setPlaceholder] = useState("Create a new todo");
+  const [isEdit, setIsEdit] = useState(false);
   const [todoItems, setTodoItems] = useState([]);
   const [error, setError] = useState(false);
   const handleInputChange = (event) => {
@@ -19,6 +20,7 @@ const Todo = () => {
         task,
         id: Math.floor(Math.random() * 100),
         completed: false,
+        edit : false
       };
 
       setTodoItems([newTask, ...todoItems]);
@@ -41,6 +43,13 @@ const Todo = () => {
     setTodoItems(newTodo);
   };
 
+  const handleEdit = (task) => {
+    const newTodo = todoItems.map((item) =>
+      item.id === task.id ? { ...item, edit: !item.edit } : item
+    );
+    setTodoItems(newTodo)
+  };
+
   return (
     <section className="w-[600px] mx-auto my-10 px-10 ">
       <form
@@ -53,12 +62,12 @@ const Todo = () => {
           className="py-3 px-2 w-3/4 bg-gray-200 capitalize outline-none"
           placeholder={placeholder}
           onFocus={() => setPlaceholder("Currently Typing")}
+          value={task}
           onBlur={() => {
             setError(false);
             setPlaceholder("Create a new Todo");
           }}
-          value={task}
-        />
+          />
         <button className="py-3 px-2 border w-1/4 uppercase rounded-lg bg-black text-white mx-1">
           add
         </button>
@@ -66,34 +75,45 @@ const Todo = () => {
       {error && <p className="text-red-500">Cannot add empty list</p>}
       <ul>
         {todoItems.map((task) => (
-          <li
-            className="flex justify-between items-center border py-3 px-2 my-3"
-            key={task.id}
-          >
-            <aside className="flex items-center">
-              <input
-                onChange={() => handleComplete(task)}
-                checked={task.completed}
-                type="checkbox"
-                name="completed"
-                id="completed"
-              />
+          <li key={task.id}>
+         
+              <section className="flex justify-between items-center border py-3 px-2 my-3">
+                <article className="flex items-center">
+                  <input
+                    onChange={() => handleComplete(task)}
+                    checked={task.completed}
+                    type="checkbox"
+                    name="completed"
+                    id="completed"
+                  />
 
-              <p className={task.completed ? "mx-2 line-through" : "mx-2"}>
-                {task.task}
-              </p>
-            </aside>
-            <div>
-              <button className="mx-2 bg-gray-200 py-2 px-4 rounded-md">
-                Edit
-              </button>
-              <button
-                onClick={() => handleDelete(task)}
-                className="mx-2 bg-gray-200 py-2 px-4 rounded-md"
-              >
-                Delete
-              </button>
-            </div>
+                  <p className={task.completed ? "mx-2 line-through" : "mx-2"}>
+                    {!task.edit ? task.task : (
+                         <input
+                        
+                         className="py-3 px-2 w-3/4 bg-gray-200 capitalize outline-none"
+                  placeholder="Edit value"
+                      
+                         />
+                    )} 
+                  </p>
+                </article>
+                <aside>
+                  <button
+                    onClick={() => handleEdit(task)}
+                    className="mx-2 bg-gray-200 py-2 px-4 rounded-md"
+                  >
+                     {!task.edit ? 'EDIT' : "EDITING" }
+                  </button>
+                  <button
+                    onClick={() => handleDelete(task)}
+                    className="mx-2 bg-gray-200 py-2 px-4 rounded-md"
+                  >
+                    Delete
+                  </button>
+                </aside>
+              </section>
+            
           </li>
         ))}
       </ul>
